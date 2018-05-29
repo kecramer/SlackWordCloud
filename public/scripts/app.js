fceea61338aee8e07d4ea033710c419412618add
maxvidrine <max.vidrine@gmail.com>
2018-05-29 15:31:06 -0700

    started hooking up channel api to dropdown

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,42 +1,36 @@
-var word_array = [
-  {text: "Hack", weight: 14},
-  {text: "Inference", weight: 15},
-  {text: "Idiot", weight: 17},
-  {text: "Idiosyncratic", weight: 9},
-  {text: "Toaster", weight: 9},
-  {text: "Couch", weight: 9},
-  {text: "Sit", weight: 7},
-  {text: "Lorem", weight: 15},
-  {text: "Ipsum", weight: 9},
-  {text: "Dolor", weight: 6},
-  {text: "Toon", weight: 9},
-  {text: "Terror", weight: 10},
-  {text: "Task", weight: 8},
-  {text: "Talent", weight: 9},
-  {text: "Tea cup", weight: 13},
-  {text: "Hasty", weight: 9},
-  {text: "Harken", weight: 8},
-  {text: "Heresy", weight: 20},
-  {text: "Hantavirus", weight: 10},
-  {text: "Amet", weight: 5}
-];
+// var word_array = [
+//   {text: "Hack", weight: 14},
+//   {text: "Inference", weight: 15},
+//   {text: "Idiot", weight: 17},
+//   {text: "Idiosyncratic", weight: 9},
+//   {text: "Toaster", weight: 9},
+//   {text: "Couch", weight: 9},
+//   {text: "Sit", weight: 7},
+//   {text: "Lorem", weight: 15},
+//   {text: "Ipsum", weight: 9},
+//   {text: "Dolor", weight: 6},
+//   {text: "Toon", weight: 9},
+//   {text: "Terror", weight: 10},
+//   {text: "Task", weight: 8},
+//   {text: "Talent", weight: 9},
+//   {text: "Tea cup", weight: 13},
+//   {text: "Hasty", weight: 9},
+//   {text: "Harken", weight: 8},
+//   {text: "Heresy", weight: 20},
+//   {text: "Hantavirus", weight: 10},
+//   {text: "Amet", weight: 5}
+// ];
 
 // set default channel
 let channel = "GAA4RPKRC";
 
 // create stopwords list
-const stopwords = ["image", "uploaded", "ios", "commented", "file", "jpg", "a", "about", "above", "across", "after", "again", "against", "all", "almost", "alone", "along", "already", "also", "although", "always", "among", "an", "and", "another", "any", "anybody", "anyone", "anything", "anywhere", "are", "area", "areas", "around", "as", "ask", "asked", "asking", "asks", "at", "away", "b", "back", "backed", "backing", "backs", "be", "became", "because", "become", "becomes", "been", "before", "began", "behind", "being", "beings", "best", "better", "between", "big", "both", "but", "by", "c", "came", "can", "cannot", "case", "cases", "certain", "certainly", "clear", "clearly", "come", "could", "d", "did", "differ", "different", "differently", "do", "does", "done", "down", "down", "downed", "downing", "downs", "during", "e", "each", "early", "either", "end", "ended", "ending", "ends", "enough", "even", "evenly", "ever", "every", "everybody", "everyone", "everything", "everywhere", "f", "face", "faces", "fact", "facts", "far", "felt", "few", "find", "finds", "first", "for", "four", "from", "full", "fully", "further", "furthered", "furthering", "furthers", "g", "gave", "general", "generally", "get", "gets", "give", "given", "gives", "go", "going", "good", "goods", "got", "great", "greater", "greatest", "group", "grouped", "grouping", "groups", "h", "had", "has", "have", "having", "he", "her", "here", "herself", "high", "high", "high", "higher", "highest", "him", "himself", "his", "how", "however", "i", "if", "important", "in", "interest", "interested", "interesting", "interests", "into", "is", "it", "its", "itself", "j", "just", "k", "keep", "keeps", "kind", "knew", "know", "known", "knows", "l", "large", "largely", "last", "later", "latest", "least", "less", "let", "lets", "like", "likely", "long", "longer", "longest", "m", "made", "make", "making", "man", "many", "may", "me", "member", "members", "men", "might", "more", "most", "mostly", "mr", "mrs", "much", "must", "my", "myself", "n", "necessary", "need", "needed", "needing", "needs", "never", "new", "new", "newer", "newest", "next", "no", "nobody", "non", "noone", "not", "nothing", "now", "nowhere", "number", "numbers", "o", "of", "off", "often", "old", "older", "oldest", "on", "once", "one", "only", "open", "opened", "opening", "opens", "or", "order", "ordered", "ordering", "orders", "other", "others", "our", "out", "over", "p", "part", "parted", "parting", "parts", "per", "perhaps", "place", "places", "point", "pointed", "pointing", "points", "possible", "present", "presented", "presenting", "presents", "problem", "problems", "put", "puts", "q", "quite", "r", "rather", "really", "right", "right", "room", "rooms", "s", "said", "same", "saw", "say", "says", "second", "seconds", "see", "seem", "seemed", "seeming", "seems", "sees", "several", "shall", "she", "should", "show", "showed", "showing", "shows", "side", "sides", "since", "small", "smaller", "smallest", "so", "some", "somebody", "someone", "something", "somewhere", "state", "states", "still", "still", "such", "sure", "t", "take", "taken", "than", "that", "the", "their", "them", "then", "there", "therefore", "these", "they", "thing", "things", "think", "thinks", "this", "those", "though", "thought", "thoughts", "three", "through", "thus", "to", "today", "together", "too", "took", "toward", "turn", "turned", "turning", "turns", "two", "u", "under", "until", "up", "upon", "us", "use", "used", "uses", "v", "very", "w", "want", "wanted", "wanting", "wants", "was", "way", "ways", "we", "well", "wells", "went", "were", "what", "when", "where", "whether", "which", "while", "who", "whole", "whose", "why", "will", "with", "within", "without", "work", "worked", "working", "works", "would", "x", "y", "year", "years", "yet", "you", "young", "younger", "youngest", "your", "yours", "z"];
+const stopwords = ["upload", "image", "uploaded", "ios", "commented", "file", "jpg", "a", "about", "above", "across", "after", "again", "against", "all", "almost", "alone", "along", "already", "also", "although", "always", "among", "an", "and", "another", "any", "anybody", "anyone", "anything", "anywhere", "are", "area", "areas", "around", "as", "ask", "asked", "asking", "asks", "at", "away", "b", "back", "backed", "backing", "backs", "be", "became", "because", "become", "becomes", "been", "before", "began", "behind", "being", "beings", "best", "better", "between", "big", "both", "but", "by", "c", "came", "can", "cannot", "case", "cases", "certain", "certainly", "clear", "clearly", "come", "could", "d", "did", "differ", "different", "differently", "do", "does", "done", "down", "down", "downed", "downing", "downs", "during", "e", "each", "early", "either", "end", "ended", "ending", "ends", "enough", "even", "evenly", "ever", "every", "everybody", "everyone", "everything", "everywhere", "f", "face", "faces", "fact", "facts", "far", "felt", "few", "find", "finds", "first", "for", "four", "from", "full", "fully", "further", "furthered", "furthering", "furthers", "g", "gave", "general", "generally", "get", "gets", "give", "given", "gives", "go", "going", "good", "goods", "got", "great", "greater", "greatest", "group", "grouped", "grouping", "groups", "h", "had", "has", "have", "having", "he", "her", "here", "herself", "high", "high", "high", "higher", "highest", "him", "himself", "his", "how", "however", "i", "if", "important", "in", "interest", "interested", "interesting", "interests", "into", "is", "it", "its", "itself", "j", "just", "k", "keep", "keeps", "kind", "knew", "know", "known", "knows", "l", "large", "largely", "last", "later", "latest", "least", "less", "let", "lets", "like", "likely", "long", "longer", "longest", "m", "made", "make", "making", "man", "many", "may", "me", "member", "members", "men", "might", "more", "most", "mostly", "mr", "mrs", "much", "must", "my", "myself", "n", "necessary", "need", "needed", "needing", "needs", "never", "new", "new", "newer", "newest", "next", "no", "nobody", "non", "noone", "not", "nothing", "now", "nowhere", "number", "numbers", "o", "of", "off", "often", "old", "older", "oldest", "on", "once", "one", "only", "open", "opened", "opening", "opens", "or", "order", "ordered", "ordering", "orders", "other", "others", "our", "out", "over", "p", "part", "parted", "parting", "parts", "per", "perhaps", "place", "places", "point", "pointed", "pointing", "points", "possible", "present", "presented", "presenting", "presents", "problem", "problems", "put", "puts", "q", "quite", "r", "rather", "really", "right", "right", "room", "rooms", "s", "said", "same", "saw", "say", "says", "second", "seconds", "see", "seem", "seemed", "seeming", "seems", "sees", "several", "shall", "she", "should", "show", "showed", "showing", "shows", "side", "sides", "since", "small", "smaller", "smallest", "so", "some", "somebody", "someone", "something", "somewhere", "state", "states", "still", "still", "such", "sure", "t", "take", "taken", "than", "that", "the", "their", "them", "then", "there", "therefore", "these", "they", "thing", "things", "think", "thinks", "this", "those", "though", "thought", "thoughts", "three", "through", "thus", "to", "today", "together", "too", "took", "toward", "turn", "turned", "turning", "turns", "two", "u", "under", "until", "up", "upon", "us", "use", "used", "uses", "v", "very", "w", "want", "wanted", "wanting", "wants", "was", "way", "ways", "we", "well", "wells", "went", "were", "what", "when", "where", "whether", "which", "while", "who", "whole", "whose", "why", "will", "with", "within", "without", "work", "worked", "working", "works", "would", "x", "y", "year", "years", "yet", "you", "young", "younger", "youngest", "your", "yours", "z"];
 
 
 
 $(document).ready(() => {
     console.log('app.js loaded!');
-
-    // update channel when dropdown selection is made
-    $('select').on('change', () => {
-        channel = $('select option:selected')[0].value;
-        console.log("Selected channel is now " + channel);
-    });
 
     handleError = (err) => {
         console.log('There has been an error: ', err);
@@ -50,6 +44,39 @@ $(document).ready(() => {
         $("#wordcloud").jQCloud(word_array);
     }
 
+
+    // // Populate list of channels in dropdown
+    // renderChannel = (channel) => {
+    //   // let username = response[0].name;
+    //   // HTML Template
+    //   const htmlToAppend = (`
+    //       <option value="${channel.slack_id}"${($('#dropdown').html() === '') ? ' selected="selected"' : ''}>${channel.name}</option>
+    //       `);
+    //   // Append each message
+    //   $('#dropdown').append(htmlToAppend);
+    // }
+
+    // renderAllChannels = (channels) => {
+    //     // Clear previous results
+    //     $('#dropdown').html('');
+    //     // Add each message
+    //     channels.forEach((channel) => {
+    //       renderChannel(channel);
+    //     });
+    // };
+
+    // $.ajax({
+    //     // event.preventDefault();
+    //     method: 'GET',
+    //     url: `/channels`,
+    //     success: (response) => {
+    //         console.log(response);
+    //         let channels = response.channels;
+    //         renderAllChannels(channels);
+    //     },
+    //     error: handleError,
+    // })    
+
     // Pull Word List (from the selected channel)
     $.ajax({
         method: 'GET',
@@ -58,19 +85,24 @@ $(document).ready(() => {
         error: handleError
     })
 
+    // update channel when dropdown selection is made
+    $('select').on('change', () => {
+        channel = $('select option:selected')[0].value;
+        initWordCloud;
+        console.log("Selected channel is now " + channel);
+    });
+
 
 
 
     // Pull messages with the selected word
 
     renderMessage = (message) => {
-        // populate user name
-        let username = '';
         $.ajax({
           method: 'GET',
           url: `/users?q=` + message.user,
           success: (response) => {
-              username = response[0].name;
+              let username = response[0].name;
               // HTML Template
               const htmlToAppend = (`
                   <div class="result">
@@ -85,7 +117,6 @@ $(document).ready(() => {
           error: handleError,
           }
         );
-        console.log(username);
 
     }
 
@@ -98,9 +129,10 @@ $(document).ready(() => {
         });
     };
 
-    let word_elements = $('span');
-    console.log(word_elements);
-    // word_elements.forEach((word) => word
+    // OnClick for each word in word cloud
+
+    // let word_elements = $('span');
+    // console.log(word_elements);
     $('#wordcloud').on('click', 'span', (event) => {
         console.log("clicked " + event.target.innerText);
         $.ajax({
@@ -116,6 +148,8 @@ $(document).ready(() => {
         })    
 
     });
+
+
 
 
 
