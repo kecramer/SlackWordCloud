cb98a65a39a276e869179aa4c2e75046a90a2e80
kecramer <kcramer@gmail.com>
2018-05-29 14:21:58 -0700

    Add ability to send users as slack ids or mongo db ids

--- controller/usersController.js
+++ controller/usersController.js
@@ -10,35 +10,60 @@ const show = (req, res) => {
 	let usersInfo = [],
 	    usersToGet = [];
 
-	usersList.forEach((user, i) => {
-		db.User.find({slack_id: user}, (err, foundUser) => {
-			if(err) {
-				console.log(err);
-				res.sendStatus(500);
-			}
+	if(usersList[0].length === 24) {
+		//We have been given mongo ids
+		usersList.forEach((user, i) => {
+			db.User.findById(user, (err, foundUser) => {
+				if(err) {
+					console.log(err);
+					res.sendStatus(500);
+				}
 
-			if(foundUser.length > 0) {
-				usersInfo.push(foundUser[0]);
-			} else {
-				usersToGet.push(user);
-			}
+				if(foundUser !== undefined) {
+					usersInfo.push(foundUser);
+				}
 
-			if(usersList.length === i + 1) {
-				foundUsers(req, res, usersInfo, usersToGet);
-			}
+				if(usersList.length === i + 1) {
+					foundUsers(req, res, usersInfo);
+				}
+			});
 		});
-	});
+	} else {
+		//We have been given slack ids
+		usersList.forEach((user, i) => {
+			db.User.find({slack_id: user}, (err, foundUser) => {
+				if(err) {
+					console.log(err);
+					res.sendStatus(500);
+				}
+
+				if(foundUser.length > 0) {
+					usersInfo.push(foundUser[0]);
+				} else {
+					usersToGet.push(user);
+				}
+
+				if(usersList.length === i + 1) {
+					foundUsers(req, res, usersInfo, usersToGet);
+				}
+			});
+		});
+	}
 };
 
 const foundUsers = (req, res, completeUsers, incompleteUsers) => {
-	slack.getUsers(incompleteUsers, (userData) => {
-		if(userData.length > 0) {
-			userData.forEach((user) => {
-				completeUsers.push(user);
-			});
-		}
+	if(incompleteUsers) {
+		slack.getUsers(incompleteUsers, (userData) => {
+			if(userData.length > 0) {
+				userData.forEach((user) => {
+					completeUsers.push(user);
+				});
+			}
+			res.json(completeUsers);
+		});
+	} else {
 		res.json(completeUsers);
-	});
+	}
 }
 
 module.exports = {
