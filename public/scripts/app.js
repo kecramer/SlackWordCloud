8302b6728cc6020aa3c80abc6efd26ffc013c7fd
maxvidrine <max.vidrine@gmail.com>
2018-05-29 16:10:54 -0700

    dropdown is now loading channels from ajax call

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -28,7 +28,6 @@ let channel = "GAA4RPKRC";
 const stopwords = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "upload", "image", "uploaded", "ios", "commented", "file", "jpg", "a", "about", "above", "across", "after", "again", "against", "all", "almost", "alone", "along", "already", "also", "although", "always", "among", "an", "and", "another", "any", "anybody", "anyone", "anything", "anywhere", "are", "area", "areas", "around", "as", "ask", "asked", "asking", "asks", "at", "away", "b", "back", "backed", "backing", "backs", "be", "became", "because", "become", "becomes", "been", "before", "began", "behind", "being", "beings", "best", "better", "between", "big", "both", "but", "by", "c", "came", "can", "cannot", "case", "cases", "certain", "certainly", "clear", "clearly", "come", "could", "d", "did", "differ", "different", "differently", "do", "does", "done", "down", "down", "downed", "downing", "downs", "during", "e", "each", "early", "either", "end", "ended", "ending", "ends", "enough", "even", "evenly", "ever", "every", "everybody", "everyone", "everything", "everywhere", "f", "face", "faces", "fact", "facts", "far", "felt", "few", "find", "finds", "first", "for", "four", "from", "full", "fully", "further", "furthered", "furthering", "furthers", "g", "gave", "general", "generally", "get", "gets", "give", "given", "gives", "go", "going", "good", "goods", "got", "great", "greater", "greatest", "group", "grouped", "grouping", "groups", "h", "had", "has", "have", "having", "he", "her", "here", "herself", "high", "high", "high", "higher", "highest", "him", "himself", "his", "how", "however", "i", "if", "important", "in", "interest", "interested", "interesting", "interests", "into", "is", "it", "its", "itself", "j", "just", "k", "keep", "keeps", "kind", "knew", "know", "known", "knows", "l", "large", "largely", "last", "later", "latest", "least", "less", "let", "lets", "like", "likely", "long", "longer", "longest", "m", "made", "make", "making", "man", "many", "may", "me", "member", "members", "men", "might", "more", "most", "mostly", "mr", "mrs", "much", "must", "my", "myself", "n", "necessary", "need", "needed", "needing", "needs", "never", "new", "new", "newer", "newest", "next", "no", "nobody", "non", "noone", "not", "nothing", "now", "nowhere", "number", "numbers", "o", "of", "off", "often", "old", "older", "oldest", "on", "once", "one", "only", "open", "opened", "opening", "opens", "or", "order", "ordered", "ordering", "orders", "other", "others", "our", "out", "over", "p", "part", "parted", "parting", "parts", "per", "perhaps", "place", "places", "point", "pointed", "pointing", "points", "possible", "present", "presented", "presenting", "presents", "problem", "problems", "put", "puts", "q", "quite", "r", "rather", "really", "right", "right", "room", "rooms", "s", "said", "same", "saw", "say", "says", "second", "seconds", "see", "seem", "seemed", "seeming", "seems", "sees", "several", "shall", "she", "should", "show", "showed", "showing", "shows", "side", "sides", "since", "small", "smaller", "smallest", "so", "some", "somebody", "someone", "something", "somewhere", "state", "states", "still", "still", "such", "sure", "t", "take", "taken", "than", "that", "the", "their", "them", "then", "there", "therefore", "these", "they", "thing", "things", "think", "thinks", "this", "those", "though", "thought", "thoughts", "three", "through", "thus", "to", "today", "together", "too", "took", "toward", "turn", "turned", "turning", "turns", "two", "u", "under", "until", "up", "upon", "us", "use", "used", "uses", "v", "very", "w", "want", "wanted", "wanting", "wants", "was", "way", "ways", "we", "well", "wells", "went", "were", "what", "when", "where", "whether", "which", "while", "who", "whole", "whose", "why", "will", "with", "within", "without", "work", "worked", "working", "works", "would", "x", "y", "year", "years", "yet", "you", "young", "younger", "youngest", "your", "yours", "z"];
 
 
-
 $(document).ready(() => {
     console.log('app.js loaded!');
 
@@ -52,37 +51,37 @@ $(document).ready(() => {
         error: handleError
     }));
 
-    // // Populate list of channels in dropdown
-    // renderChannel = (channel) => {
-    //   // let username = response[0].name;
-    //   // HTML Template
-    //   const htmlToAppend = (`
-    //       <option value="${channel.slack_id}"${($('#dropdown').html() === '') ? ' selected="selected"' : ''}>${channel.name}</option>
-    //       `);
-    //   // Append each message
-    //   $('#dropdown').append(htmlToAppend);
-    // }
+    // Populate list of channels in dropdown
+    renderChannel = (channel) => {
+      // let username = response[0].name;
+      // HTML Template
+      const htmlToAppend = (`
+          <option value="${channel.slack_id}"${($('#select').html() === '') ? ' selected="selected"' : ''}>${channel.name}</option>
+          `);
+      // Append each message
+      $('#select').append(htmlToAppend);
+    }
 
-    // renderAllChannels = (channels) => {
-    //     // Clear previous results
-    //     $('#dropdown').html('');
-    //     // Add each message
-    //     channels.forEach((channel) => {
-    //       renderChannel(channel);
-    //     });
-    // };
+    renderAllChannels = (channels) => {
+        // Clear previous results
+        $('#select').html('');
+        // Add each message
+        channels.forEach((channel) => {
+          renderChannel(channel);
+        });
+    };
 
-    // $.ajax({
-    //     // event.preventDefault();
-    //     method: 'GET',
-    //     url: `/channels`,
-    //     success: (response) => {
-    //         console.log(response);
-    //         let channels = response.channels;
-    //         renderAllChannels(channels);
-    //     },
-    //     error: handleError,
-    // })    
+    $.ajax({
+        // event.preventDefault();
+        method: 'GET',
+        url: `/channels`,
+        success: (response) => {
+            console.log(response);
+            let channels = response.channels;
+            renderAllChannels(channels);
+        },
+        error: handleError,
+    })
 
     pullWordList();
     
@@ -91,7 +90,7 @@ $(document).ready(() => {
         channel = $('select option:selected')[0].value;
         $('#wordcloud').html('');
         pullWordList();
-        console.log("Selected channel is now " + channel);
+        console.log("Selected channel is now " + $('select option:selected')[0].innerText);
     });
 
 
