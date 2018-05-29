b1ba1e69a0465211783d578e4fef35891f4af349
kecramer <kcramer@gmail.com>
2018-05-29 11:09:04 -0700

    Add endpoint to grab channels

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.get('/', (req, res) => {
 
 app.get('/channel/:id/words', controller.words.show);
 app.get('/channel/:id/messages', controller.messages.index);
+app.get('/channels', controller.channels.index);
 
 app.listen(process.env.PORT || 3000, () => {
    console.log('Express started on port ' + (process.env.PORT || '3000'));
