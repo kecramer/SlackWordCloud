2310eab5b00669b05e96aa55d7c8fc124303a022
maxvidrine <max.vidrine@gmail.com>
2018-05-31 15:08:04 -0700

    word results display previously saved messages correctly and saving and unsaving works

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,6 @@
 // set default channel
 let channel = "GAA4RPKRC";
+let savedMessages = [];
 
 // create stopwords list
 const stopwords = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "upload", "image", "uploaded", "ios", "commented", "file", "jpg", "a", "about", "above", "across", "after", "again", "against", "all", "almost", "alone", "along", "already", "also", "although", "always", "among", "an", "and", "another", "any", "anybody", "anyone", "anything", "anywhere", "are", "area", "areas", "around", "as", "ask", "asked", "asking", "asks", "at", "away", "b", "back", "backed", "backing", "backs", "be", "became", "because", "become", "becomes", "been", "before", "began", "behind", "being", "beings", "best", "better", "between", "big", "both", "but", "by", "c", "came", "can", "cannot", "case", "cases", "certain", "certainly", "clear", "clearly", "come", "could", "d", "did", "differ", "different", "differently", "do", "does", "done", "down", "down", "downed", "downing", "downs", "during", "e", "each", "early", "either", "end", "ended", "ending", "ends", "enough", "even", "evenly", "ever", "every", "everybody", "everyone", "everything", "everywhere", "f", "face", "faces", "fact", "facts", "far", "felt", "few", "find", "finds", "first", "for", "four", "from", "full", "fully", "further", "furthered", "furthering", "furthers", "g", "gave", "general", "generally", "get", "gets", "give", "given", "gives", "go", "going", "good", "goods", "got", "great", "greater", "greatest", "group", "grouped", "grouping", "groups", "h", "had", "has", "have", "having", "he", "her", "here", "herself", "high", "high", "high", "higher", "highest", "him", "himself", "his", "how", "however", "i", "if", "important", "in", "interest", "interested", "interesting", "interests", "into", "is", "it", "its", "itself", "j", "just", "k", "keep", "keeps", "kind", "knew", "know", "known", "knows", "l", "large", "largely", "last", "later", "latest", "least", "less", "let", "lets", "like", "likely", "long", "longer", "longest", "m", "made", "make", "making", "man", "many", "may", "me", "member", "members", "men", "might", "more", "most", "mostly", "mr", "mrs", "much", "must", "my", "myself", "n", "necessary", "need", "needed", "needing", "needs", "never", "new", "new", "newer", "newest", "next", "no", "nobody", "non", "noone", "not", "nothing", "now", "nowhere", "number", "numbers", "o", "of", "off", "often", "old", "older", "oldest", "on", "once", "one", "only", "open", "opened", "opening", "opens", "or", "order", "ordered", "ordering", "orders", "other", "others", "our", "out", "over", "p", "part", "parted", "parting", "parts", "per", "perhaps", "place", "places", "point", "pointed", "pointing", "points", "possible", "present", "presented", "presenting", "presents", "problem", "problems", "put", "puts", "q", "quite", "r", "rather", "really", "right", "right", "room", "rooms", "s", "said", "same", "saw", "say", "says", "second", "seconds", "see", "seem", "seemed", "seeming", "seems", "sees", "several", "shall", "she", "should", "show", "showed", "showing", "shows", "side", "sides", "since", "small", "smaller", "smallest", "so", "some", "somebody", "someone", "something", "somewhere", "state", "states", "still", "still", "such", "sure", "t", "take", "taken", "than", "that", "the", "their", "them", "then", "there", "therefore", "these", "they", "thing", "things", "think", "thinks", "this", "those", "though", "thought", "thoughts", "three", "through", "thus", "to", "today", "together", "too", "took", "toward", "turn", "turned", "turning", "turns", "two", "u", "under", "until", "up", "upon", "us", "use", "used", "uses", "v", "very", "w", "want", "wanted", "wanting", "wants", "was", "way", "ways", "we", "well", "wells", "went", "were", "what", "when", "where", "whether", "which", "while", "who", "whole", "whose", "why", "will", "with", "within", "without", "work", "worked", "working", "works", "would", "x", "y", "year", "years", "yet", "you", "young", "younger", "youngest", "your", "yours", "z", "channel", "joined", "left"];
@@ -30,9 +31,8 @@ $(document).ready(() => {
 
     // Populate list of channels in dropdown
     renderChannel = (channel) => {
-        // let username = response[0].name;
         // HTML Template
-        const htmlToAppend = (`
+        let htmlToAppend = (`
             <option value="${channel.slack_id}"${($('#select').html() === '') ? ' selected="selected"' : ''}>${channel.name}</option>
             `);
         // Append each message
@@ -49,7 +49,6 @@ $(document).ready(() => {
     };
 
     $.ajax({
-        // event.preventDefault();
         method: 'GET',
         url: `/channels`,
         success: (response) => {
@@ -83,15 +82,18 @@ $(document).ready(() => {
             method: 'GET',
             url: `/users?q=` + message.user,
             success: (response) => {
-                let username = response[0].name ? response[0].name : "A ghost of slack past";
-                let datetime = new Date(message.timestamp);
+            	const messageId = message._id;
+                const username = response[0].name ? response[0].name : "A ghost of slack past";
+                const datetime = new Date(message.timestamp);
+                let saveStatus = savedMessages.indexOf(messageId) >= 0 ? 'Unsave' : 'Save';
+                
                 // HTML Template
-                let htmlToAppend = (`
-                    <div id="${message._id}" class="result">
+                const htmlToAppend = (`
+                    <div data-msg-id="${messageId}" class="result">
                         <h4 title="${datetime.toLocaleTimeString()}">${datetime.toLocaleDateString()}</h4>
                         <h3>${username}</h3>
                         <p>${slackdown.parse(message.text)}</p>
-                        <a href="#"><h5>Save</h5></a>
+                        <a href="#"><h5>${saveStatus}</h5></a>
                     </div>
                     `);
 
@@ -102,13 +104,25 @@ $(document).ready(() => {
                 $('#results .result:last-child').on('click', 'a>h5', (event) => {
                     event.preventDefault();
 
+		            // Establish variables
+		            const wordCloudButton = event.target;
+		            let saveState = wordCloudButton.innerText;
+		            const wordCloudCopy = $(wordCloudButton).parent().parent();
+		            const savedMessageCopy = wordCloudCopy.clone();
+
                     // function to DELETE message from backend
-                    let deleteFromBackEnd = () => {
+                    const deleteFromBackEnd = () => {
                         $.ajax({
                             method: 'DELETE',
-                            url: `/messages/${message._id}`,
+                            url: `/messages/${messageId}`,
                             success: function(message) {
                                 console.log("message deleted from saved in database");
+
+                                // delete message from savedMessages array
+                                let index = savedMessages.indexOf(messageId);
+								if (index > -1) {
+								  savedMessages.splice(index, 1);
+								}
                             },
                             error: handleError
                         });
@@ -116,10 +130,12 @@ $(document).ready(() => {
                     }
 
                     // function to remove message
-                    let removeMessage = () => {
+                    const removeMessage = () => {
                         console.log("clicked to unsave message");
+
                         // DELETE message from frontend
-                        savedMessageCopy.remove();
+                        $('#savedmessages>div').attr('data-msg-id', messageId).remove();
+
                         // DELETE message from backend
                         deleteFromBackEnd()
                         saveState = 'Save'
@@ -127,37 +143,33 @@ $(document).ready(() => {
                     }
 
                     // Update Save State
-                    let wordCloudButton = event.target; // not jquery
-                    let saveState = wordCloudButton.innerText;
-                    let wordCloudCopy = $(wordCloudButton).parent().parent(); // jquery
-                    let savedMessageCopy = wordCloudCopy.clone();
                     if (saveState === 'Save') {
-					console.log("clicked to save message");
+						console.log("clicked to save message");
 
-					// POST message to backend
-					$.ajax({
-					    method: 'POST',
-					    url: `/messages/${message._id}`,
-					    data: `{message: ${message._id}}`,
-					    success: function(message) {
-					        console.log("message saved to database");
-					    },
-					    error: handleError
-					});
+						// POST message to backend
+						$.ajax({
+						    method: 'POST',
+						    url: `/messages/${messageId}`,
+						    data: `{message: ${messageId}}`,
+						    success: function(message) {
+						        console.log("message saved to database");
+						        savedMessages.push(messageId);
+						    },
+						    error: handleError
+						});
 
-					// Add message to saved tab
-					savedMessageCopy.appendTo('#savedmessages');
+						// Add message to saved tab
+						savedMessageCopy.appendTo('#savedmessages');
 
-					// Add OnClick to savedMessageCopy for removal
-					$('#savedmessages:last-child').on('click', 'a>h5', (event) => {
-				        event.preventDefault();
-				        removeMessage();
-					})
+						// Add OnClick to savedMessageCopy for removal
+						$(savedMessageCopy).on('click', (event) => {
+					        event.preventDefault();
+					        removeMessage();
+						})
 
-					saveState = 'Unsave';
-					wordCloudButton.innerText = saveState;
-					// console.log(savedMessageCopy.find('h5')[0].innerHTML);
-					savedMessageCopy.find('h5')[0].innerHTML = saveState;
+						saveState = 'Unsave';
+						wordCloudButton.innerText = saveState;;
+						savedMessageCopy.find('h5')[0].innerHTML = saveState;
                     } else {
                         console.log("clicked to unsave message");
                         removeMessage();
@@ -197,7 +209,6 @@ $(document).ready(() => {
     $('#wordcloud').on('click', 'span', (event) => {
         console.log("clicked " + event.target.innerText);
         $.ajax({
-            // event.preventDefault();
             method: 'GET',
             url: `/channel/${channel}/messages?q=` + event.target.innerText,
             success: (response) => {
